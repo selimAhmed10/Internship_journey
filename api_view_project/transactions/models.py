98f2101d589417869b5f45554296081095a74c30
Selim Ahmed <selim174662@gmail.com>
2026-06-05 19:45:07 +0600

    feat: add the transaction modelwith unique id,status and othersattributes

--- api_view_project/transactions/models.py
+++ api_view_project/transactions/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from accounts.models import Account
+import uuid
 
-# Create your models here.
+class Transaction(models.Model):
+    types=[
+        ('Credit','credit'),
+        ('Debit','debit'),
+    ]
+    transaction_status=[
+        ('Success','success'),
+        ('Failed','failed'),
+        ('Pending','Pending'),
+    ]
+    
+    trans_id=models.UUIDField(primary_key =True,default=uuid.uuid4,editable=False)
+    account=models.ForeignKey(Account,on_delete=models.CASCADE,related_name='transactions')
+    amount=models.DecimalField(max_digits=12,decimal_places=2)
+    transaction_type=models.CharField(max_length=10,choices=types)
+    status=models.CharField(max_length=10,choices=transaction_status,default='pending')
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.trans_id}"
