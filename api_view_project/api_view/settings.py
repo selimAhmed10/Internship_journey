16b3d0971d6e78b58703d3f5f54989e8e1918d8e
Selim Ahmed <selim174662@gmail.com>
2026-06-05 11:24:51 +0600

    Configure settings: add apps, REST framework

--- api_view_project/api_view/settings.py
+++ api_view_project/api_view/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'accounts',
+    'transactions',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny'],
+}
