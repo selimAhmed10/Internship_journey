324b6d35a2fb56a2424dbc08fe27d72c3aadb969
Selim Ahmed <selim174662@gmail.com>
2026-06-05 15:07:25 +0600

    feat:add Account model with primary key,account type,choices, balance and others essential

--- api_view_project/accounts/models.py
+++ api_view_project/accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import uuid  #make the unique id number that nobody earlier create 
 
-# Create your models here.
+class Account(models.Model):
+    account_type=[
+        ('Saving','saving'),
+        ('Current','current'),
+        ('student','student')
+       
+    ]
+    
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    account_number=models.CharField(max_length=20,unique=True)
+    account_owner=models.CharField(max_length=30)
+    account_type=models.CharField(choices=account_type,max_length=50)
+    balance=models.DecimalField(max_digits=15,decimal_places=2,default=0)
+    is_frozen=models.BooleanField(default=False)
+    is_active=models.BooleanField(default=True)
+    create_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.account_number}"
