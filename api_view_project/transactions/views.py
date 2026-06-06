f6b3637a0aee97bdc9357b6bd38191d9f59bb879
Selim Ahmed <selim174662@gmail.com>
2026-06-06 11:11:23 +0600

    feat:implement TransactionAPIView with full CRUD operations using APIView

--- api_view_project/transactions/views.py
+++ api_view_project/transactions/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Transaction
+from rest_framework.response import Response
+from .serializers import TransactionSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class TransactionAPIView(APIView):
+    def get(self,request,pk=None):
+        if pk:
+            transaction=get_object_or_404(Transaction,pk=pk)
+            serializer=TransactionSerializer(transaction)
+            return Response(serializer.data)
+
+        transactions=Transaction.objects.all()
+        serializer=TransactionSerializer(transactions,many=True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer=TransactionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def put(self,request,pk):
+        transaction=get_object_or_404(Transaction,pk=pk)
+        serializer=TransactionSerializer(transaction, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def patch(self,request,pk):
+        transaction=get_object_or_404(Transaction,pk=pk)
+        serializer=TransactionSerializer(transaction,data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def delete(self,request,pk):
+        transaction=get_object_or_404(Transaction,pk=pk)
+        transaction.delete()
+        return Response({"message": "successfully deleted"})
+
