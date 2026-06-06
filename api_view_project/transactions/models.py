b63bb6ae0202564dfc521a4f68494c3ebb92912e
Selim Ahmed <selim174662@gmail.com>
2026-06-06 12:54:12 +0600

    Fix: update Transaction model choices and status defaults

--- api_view_project/transactions/models.py
+++ api_view_project/transactions/models.py
@@ -4,20 +4,21 @@ import uuid
 
 class Transaction(models.Model):
     types=[
-        ('Credit','credit'),
-        ('Debit','debit'),
-    ]
+        ('credit','Credit'),
+        ('debit','Debit'),
+        ]
+    
     transaction_status=[
-        ('Success','success'),
-        ('Failed','failed'),
-        ('Pending','Pending'),
-    ]
+        ('success','Success'),
+        ('failed','Failed'),
+        ('pending','Pending'),
+        ]
     
     trans_id=models.UUIDField(primary_key =True,default=uuid.uuid4,editable=False)
     account=models.ForeignKey(Account,on_delete=models.CASCADE,related_name='transactions')
     amount=models.DecimalField(max_digits=12,decimal_places=2)
     transaction_type=models.CharField(max_length=10,choices=types)
-    status=models.CharField(max_length=10,choices=transaction_status,default='pending')
+    status = models.CharField(max_length=10, choices=transaction_status, default='pending')
     created_at=models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
