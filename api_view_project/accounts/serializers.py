e86fa44f1cb78d4824073e6ac44a5e5c5ba31a35
Selim Ahmed <selim174662@gmail.com>
2026-06-10 07:24:33 +0600

    feat: add serializer for showing the list with important info in a list and another for the details there have all field

--- api_view_project/accounts/serializers.py
+++ api_view_project/accounts/serializers.py
@@ -5,3 +5,21 @@ class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
         fields = '__all__'
+        
+        
+class AccountListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Account
+        fields =['id','account_number','account_owner','balance']
+        #here i can use also the exclude 
+        #for adding new field can use - serializerFieldMethod()
+        #can have read only or can use kwarg for add more behaviour like required or not 
+        #can use torepresentation,writeonly,tointernalvalue and more 
+        #can use validation and more can be custom also 
+
+
+class AccountDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Account
+        fields ='__all__'
+        read_only_fields =['id','create_at']
