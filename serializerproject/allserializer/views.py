512657a23dd51877ae054fe3b314f0da0d56dd09
Md Selim Ahmed <selim174662@gmail.com>
2026-06-18 11:03:10 +0600

    feat: add the AcoountModelserializerApiview with the method of full crud using the AccountModelSerializer

--- serializerproject/allserializer/views.py
+++ serializerproject/allserializer/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User,Account,Transaction
-from .serializers import BasicUserSerializer
+from .serializers import BasicUserSerializer,AccountModelSerializer
 from django.shortcuts import get_object_or_404
 
 
@@ -27,6 +27,46 @@ class BasicUserAPIView(APIView):
             serializer.save()
             return Response(serializer.data,status=201)
         return Response(serializer.errors,status=400)
+    
+
+
+class AccountModelSerializerAPIView(APIView):
+    def get(self,request,pk=None):
+        if pk:
+            account= get_object_or_404(Account,pk=pk)
+            serializer=AccountModelSerializer(account)
+            return Response(serializer.data)
         
-        
-     
+        accounts=Account.objects.all()
+        serializer=AccountModelSerializer(accounts,many=True)
+        return Response(serializer.data)
+    
+    
+    def post(self,request):
+        serializer=AccountModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self,request,pk):
+        account=get_object_or_404(Account,pk=pk)
+        serializer=AccountModelSerializer(account,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self,request,pk):
+        account=get_object_or_404(Account,pk=pk)
+        serializer=AccountModelSerializer(account,data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request,pk):
+        account=get_object_or_404(Account,pk=pk)
+        account.delete()
+        return Response({"message":"deleted successfully"})
