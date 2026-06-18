d07f65e664a46bfd1b327884221c547d3c11b384
Md Selim Ahmed <selim174662@gmail.com>
2026-06-18 11:49:05 +0600

    feat: add the readOnlySerializer class for just read the trasactions one by id or all user TrasactionReadOnlyserializer

--- serializerproject/allserializer/views.py
+++ serializerproject/allserializer/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User,Account,Transaction
-from .serializers import BasicUserSerializer,AccountModelSerializer,UserHyperlinkSerializer
+from .serializers import BasicUserSerializer,AccountModelSerializer,UserHyperlinkSerializer,TrasactionReadOnlySerializer
 from django.shortcuts import get_object_or_404
 
 
@@ -85,6 +85,19 @@ class UserListCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+class ReadOnlyTransaction(APIView):
+    def get(self,request,pk=None):
+        if pk:
+            transaction=get_object_or_404(Transaction,pk=pk)
+            serializer=TrasactionReadOnlySerializer(transaction)
+            return Response(serializer.data)
+        
+        transactions=Transaction.objects.all()
+        serializer=TrasactionReadOnlySerializer(transactions,many=True)
+        return Response(serializer.data)
 
     
     
