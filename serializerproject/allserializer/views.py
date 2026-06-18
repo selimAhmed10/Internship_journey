bd9a82067c2536cd01b3e3283dad5cc7b1e1b01f
Md Selim Ahmed <selim174662@gmail.com>
2026-06-18 10:50:10 +0600

    feat: add the basicUserApiView class with the method of create,get and update with status code using BasicUserSerializer

--- serializerproject/allserializer/views.py
+++ serializerproject/allserializer/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User,Account,Transaction
+from .serializers import BasicUserSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class BasicUserAPIView(APIView):
+    def get(self,request):
+        users=User.objects.all()
+        serializer=BasicUserSerializer(users,many=True)
+        return Response(serializer.data)
+    
+    
+    def post(self,request):
+        serializer=BasicUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.error_messages("not suceessfull to craete the data"),status=400)
+
+    def put(self,request,pk):
+        user=get_object_or_404(User,pk=pk)
+        serializer=BasicUserSerializer(user,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.errors,status=400)
+        
+        
+     
