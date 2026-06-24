fd5726ce61e5648f994fef74d9b6e26be3292fa6
Md Selim Ahmed <selim174662@gmail.com>
2026-06-24 11:44:35 +0600

    feat : add the UserRegisterAPIView class to register user with essential fields

--- jwtproject/users/views.py
+++ jwtproject/users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from rest_framework import status
+from .serializers import UserRegisterSerializer,UserLoginSerializer
 
-# Create your views here.
+class UserResgisterAPIView(APIView):
+    
+    permission_classes=[AllowAny]  # anyone can do login 
+    def post(self,request):
+        register_serializer=UserRegisterSerializer(data=request.data)
+        if register_serializer.is_valid():
+            user=register_serializer.save()
+       
+            return Response({
+                "status":"success",
+                "message":"User successfully created",
+                "data":{
+                    "id":str(user.id),
+                    "email":user.email,
+                    "phone":user.phone,
+                    "address":user.address,
+                    "role":user.role
+                }
+            },status=status.HTTP_201_CREATED)
+        return Response(register_serializer.errors)  
