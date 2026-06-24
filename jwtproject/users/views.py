84cd5f5029406f7ccedd3d7a098b0a9e6b16fe44
Md Selim Ahmed <selim174662@gmail.com>
2026-06-24 11:55:01 +0600

    feat : add the UserLoginAPIView class to login using the email and password and if complete show the info and token for others page access

--- jwtproject/users/views.py
+++ jwtproject/users/views.py
@@ -24,3 +24,26 @@ class UserResgisterAPIView(APIView):
                 }
             },status=status.HTTP_201_CREATED)
         return Response(register_serializer.errors)  
+
+
+class UserLoginAPIView(APIView):
+    permission_classes=[AllowAny]  #everyrole and anyone can try login using the email and the password 
+    def post(self,request):
+        serializer=UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            data=serializer.validated_data 
+            return Response({
+                "status":"success",
+                "message":"User login successfull",
+                "data": {
+                    "user": {
+                        "id":str(data["user"].id), # receive the data from the serializer and access it to show user info
+                        "email":data["user"].email,
+                        "role":data["user"].role
+                    },
+                    "access":data["access"],  # access and refrsh token create in the serializer class here just access and return it to the user 
+                    "refresh":data["refresh"]
+                }
+            },status=status.HTTP_200_OK)
+        return Response(serializer.errors)
+     
