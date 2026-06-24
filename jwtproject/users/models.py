cdee55722e9a34ffb7299927f0cbd3115013cbbf
Md Selim Ahmed <selim174662@gmail.com>
2026-06-24 14:40:39 +0600

    feat(model): add the Session model for tracking the user all activies and can take action like revoke and others

--- jwtproject/users/models.py
+++ jwtproject/users/models.py
@@ -1,3 +1,4 @@
+from datetime import timezone
 import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser,BaseUserManager
@@ -8,7 +9,7 @@ class UserManager(BaseUserManager):
     def create_user(self,email,password=None,**extra_fields):
         if not email:
             raise ValueError("the email is required for register")
-        email=self.normalize_email(email)   # make the email normalize by doint it lower case 
+        email=self.normalize_email(email)# make the email normalize by doint it lower case 
         if 'username' not in extra_fields or not extra_fields['username']:extra_fields['username']=email   # if the field dont have the username take email as the user name for it 
         user=self.model(email=email,**extra_fields)
         user.set_password(password)
@@ -68,3 +69,40 @@ class User(AbstractUser):
     @property
     def is_merchant(self):
         return self.role=="merchant"
+
+
+
+
+
+class UserSession(models.Model):
+    session_id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='session')
+    access_token_jti=models.CharField(max_length=350,db_index=True) #token identifier 
+    refresh_token_jti=models.CharField(max_length=250,db_index=True)
+    device_name=models.CharField(max_length=255,default="unknown_device")
+    browser_name=models.CharField(max_length=255,default='Uknown_browser')
+    ip_address=models.GenericIPAddressField(null=True,blank=True)
+    user_agent=models.TextField(blank=True)
+    login_time=models.DateTimeField(auto_now_add=True)
+    last_activity=models.DateTimeField(auto_now=True)  #last hit on the api
+    expires_at=models.DateTimeField() #expire time
+    is_active=models.BooleanField(default=True)
+    is_blacklisted=models.BooleanField(default=False)
+    
+    class Meta:
+        db_table="user_session"
+        ordering=["-login_time"]
+        indexes=[
+            models.Index(fields=["user", "is_active"]),
+        ]
+    
+    def __str__(self):
+        return f"{self.user.email} ---- {self.device_name} --- {self.login_time}"
+    
+    def revoke(self): #for revoke it make is active false and mark blacklist true 
+        self.is_active=False
+        self.is_blacklisted=True
+        self.save(update_fields=["is_active","is_blacklisted"]) #then save this fields 
+    
+    def is_expired(self):
+        return timezone.now()>self.expires_at  #check with the timezone now to check and compare session is expired or not 
