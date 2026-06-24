1936fded53341f3dd84da4cf6e09c668625be030
Md Selim Ahmed <selim174662@gmail.com>
2026-06-24 13:21:56 +0600

    feat: add the api url for the logout and the token rotation

--- jwtproject/users/urls.py
+++ jwtproject/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserResgisterAPIView,UserLoginAPIView,DashboardAPIView
+from .views import UserResgisterAPIView,UserLoginAPIView,DashboardAPIView,LogOutAPIView,TokenRefreshAPIView
 urlpatterns = [
     path('register/',UserResgisterAPIView.as_view(),name='register'),
     path('login/',UserLoginAPIView.as_view(),name='login'),
     path('dashboard/',DashboardAPIView.as_view(),name='dashboard'),
+    path('refresh_token/',TokenRefreshAPIView.as_view(),name='refresh_rotation'),
+    path('logout/',LogOutAPIView.as_view(),name='logout')
 ]
