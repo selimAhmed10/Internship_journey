a1b01e95dbfb187e7dbc65fd95de3120646a9fc7
Md Selim Ahmed <selim174662@gmail.com>
2026-06-24 10:47:26 +0600

    feat: add the UserLoginSerializer with validattion username and password then token generation

--- jwtproject/users/serializers.py
+++ jwtproject/users/serializers.py
@@ -1,6 +1,8 @@
 from .models import User
 from rest_framework import serializers
-
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from django.shortcuts import get_object_or_404
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
@@ -20,3 +22,24 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         return User.objects.create_user(**validated_data)
 
 
+class UserLoginSerializer(serializers.Serializer):
+    email=serializers.EmailField()
+    password=serializers.CharField(write_only=True)
+    
+    def validate(self, attrs):
+        email=attrs.get('email')
+        password=attrs.get('password')
+        user=get_object_or_404(User,email__iexact=email)
+        
+        user=authenticate(email=email,password=password)
+        if not user:
+            raise serializers.ValidationError("Invalid password")
+        
+        refresh=RefreshToken.for_user(user)
+        return {
+            'user':user,
+            'email':user.email,
+            'role':user.role,
+            'access':str(refresh.access_token),
+            'refresh':str(refresh),
+        }
