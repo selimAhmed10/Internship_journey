223b3890f376b1f8339e940121c84731f174848d
Md Selim Ahmed <selim174662@gmail.com>
2026-06-25 13:20:44 +0600

    fix: add the custom middleware file path in the settings to store all api hit info including name,ip etc

--- jwtproject/jwt_project/settings.py
+++ jwtproject/jwt_project/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'jwt_project.middleware.APITrackingMiddleware',
 ]
 
 ROOT_URLCONF = 'jwt_project.urls'
