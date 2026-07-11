73d40509a8b0ac67d44eb20983d361adf3fbb926
Selim Ahmed <selim174662@gmail.com>
2026-07-11 22:06:53 +0600

    feat: add the admin dashboard class ,only access the admin and see his dashbaord with all info

--- jwtpermissionproject/transactions/views.py
+++ jwtpermissionproject/transactions/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.db import transaction
+from django.db.models import F, Q
+from .models import Transaction,User
+from .serializer import TrasactionSerializer,CashInSerializer,CashOutSerializer,SendMoneySerializer
+from permissions.custom_permissions import IsAdmin, IsAgent, IsCustomer
 
-# Create your views here.
+class AdminDashboard(APIView):
+    permission_classes=[IsAdmin]
+    
+    def get(self,request):
+        user= request.user
+        total_customer=User.objects.filter(role='customer').count()
+        total_agents=User.objects.filter(role='agent').count()
+        total_transaction=Transaction.objects.count()
+        return Response({
+            'name': user.full_name,
+            'role': user.role,
+            'email': user.email,
+            'total_customers': total_customer,
+            'total_agents': total_agents,
+            'total_transactions':total_transaction,
+        })
