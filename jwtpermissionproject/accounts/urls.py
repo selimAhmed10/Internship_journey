f7ce1952c7a3e9117419c85eefb433ca4e3d9c5f
Selim Ahmed <selim174662@gmail.com>
2026-07-12 00:23:24 +0600

    feat: add the crud operation all api enspoints urls

--- jwtpermissionproject/accounts/urls.py
+++ jwtpermissionproject/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import UserloginAPIView,UserResgistrationAPIView,LogOutAPIview
+from .views import AdminUserListView, UserManageView, UserloginAPIView,UserResgistrationAPIView,LogOutAPIview
 urlpatterns=[
     path('register/', UserResgistrationAPIView.as_view(),name='register'),
     path('login/', UserloginAPIView.as_view(),name='login'),
-    path('refresh/', TokenRefreshView.as_view(),name='token_refresh'),
+    path('refresh/',TokenRefreshView.as_view(),name='token_refresh'),
     path('logout/', LogOutAPIview.as_view(),name='logout'),
+    path('admin/users/list/',AdminUserListView.as_view(), name='admin-users-list'),
+    path('admin/users/', UserManageView.as_view(), name='admin-users'),
+    path('admin/users/<uuid:user_id>/', UserManageView.as_view(), name='admin-user-detail'),
 ]
