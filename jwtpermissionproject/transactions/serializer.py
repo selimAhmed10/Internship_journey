90fa3455ad564b12c0e0509b27a7d74c2ecfb818
Selim Ahmed <selim174662@gmail.com>
2026-07-11 21:16:55 +0600

    feat: add the cashin serializer and a method to check the reciver phone number validate and exist , then check role and in serializer check the balance sufficient or not

---
+++ jwtpermissionproject/transactions/serializer.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+from .models import Transaction,User
+import re
+
+class TrasactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Transaction
+        fields='__all__'
+        read_only_fields=['id','transaction_id','status','timestamp']
+
+def validate_and_get_user(phone,role=None,exclude_user=None):
+    if not phone:
+        raise serializers.ValidationError("phone number is required")
+    phone=phone.strip()
+    if phone.startwith('+8801'):
+        phone=phone[3:]
+    elif phone.startwith('8801'):
+        phone=phone[2:]
+    if not re.match(r'^01[3-9]\d{8}$',phone):
+        raise serializers.ValidationError("the phone number must startwith 01 and must 11")
+    
+    try:
+        user=User.objects.get(phone_number_wallet_number=phone, is_active=True)
+    except User.DoesNotExist:
+        raise serializers.ValidationError("No user found using the number ")
+    
+    if role and user.role!=role:
+        raise serializers.ValidationError(f"User is not a {role}")
+    
+    if exclude_user and user==exclude_user:
+        raise serializers.ValidationError("you cant do operation in your own number")
+    
+    return user
+
+
+class CashInSerializer(serializers.ModelSerializer):
+    customer_phone=serializers.CharField(required=True)
+    amount=serializers.DecimalField(required=True,max_digits=15,decimal_places=2,min_value=10)
+    
+    def validate_customer_phone(self,value):
+        user=validate_and_get_user(value,role='customer')
+        return user.phone_number_wallet_number
+    
+    def validate(self,attrs):
+        agent=self.context['request'].user
+        if agent.balance<attrs['amount']:
+            raise serializers.ValidationError(f"insufficient balance ,now amount {agent.balance}")
+        return attrs
+    
+    
