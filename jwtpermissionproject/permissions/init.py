28288840789889a179f3ce5943412bf2e665dac4
Md Selim Ahmed <selim174662@gmail.com>
2026-07-12 13:43:21 +0600

    feat: add the custom permission class in the init permission to work when start server

--- jwtpermissionproject/permissions/init.py
+++ jwtpermissionproject/permissions/init.py
@@ -1,4 +1,4 @@
 # permissions/__init__.py
-from .custom_permissions import IsAdmin,IsAgent,IsCustomer
+from .custom_permissions import IsAdmin,IsAgent,IsCustomer,IsTransactionOwner
 
-__all__ = ['IsAdmin','IsAgent','IsCustomer',]
+__all__ = ['IsAdmin','IsAgent','IsCustomer','IsTransactionOwner',]
